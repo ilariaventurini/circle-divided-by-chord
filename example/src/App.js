903486aad33929cc4c7ca74f2394c487b9288b4a
Ilaria Venturini <ilaria.venturini.ext@accurat.it>
2019-09-12 14:14:17 +0200

    Create example

--- example/src/App.js
+++ example/src/App.js
@@ -3,10 +3,6 @@ import CircleDividedByChord from "circle-divided-by-chord";
 
 export default class App extends Component {
   render() {
-    return (
-      <div className="">
-        <CircleDividedByChord radius={300} datum1={30} datum2={70} />
-      </div>
-    );
+    return <CircleDividedByChord />;
   }
 }
